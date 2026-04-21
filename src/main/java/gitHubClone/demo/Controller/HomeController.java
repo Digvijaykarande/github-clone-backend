9aa3495bcc4eb7bd0182f5a37759f9bbb54370df
Digvijaykarande <karandedigvijay2@gmail.com>
2026-04-21 22:10:44 +0530

    fake controllder added

--- src/main/java/gitHubClone/demo/Controller/HomeController.java
+++ src/main/java/gitHubClone/demo/Controller/HomeController.java
@@ -1,12 +1,20 @@
 package gitHubClone.demo.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HomeController {
 
 	@RequestMapping("/")
 	public String greet() {
 		return "helow ";
 	}
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("ok");
+    }
 }
