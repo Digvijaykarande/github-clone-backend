08fd049cd2568b0566e2fa20ba23ce95fe88bb36
Digvijaykarande <karandedigvijay2@gmail.com>
2026-04-22 13:35:09 +0530

    server arealy wakeup route added

--- src/main/java/gitHubClone/demo/Controller/HomeController.java
+++ src/main/java/gitHubClone/demo/Controller/HomeController.java
@@ -4,15 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 @RestController
 @RequestMapping("/api")
 public class HomeController {
 
-	@RequestMapping("/")
-	public String greet() {
-		return "helow ";
-	}
+    @RequestMapping("/")
+    public String greet() {
+        return "helow ";
+    }
 
+    @CrossOrigin(origins = "*")  
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
         return ResponseEntity.ok("ok");
